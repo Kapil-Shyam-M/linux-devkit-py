342b87a454ee3feb3feda255175a473b9a52eff9
Kapil Shyam M <81209388+Kapil-Shyam-M@users.noreply.github.com>
2022-03-05 18:58:20 +0530

    Update pk.c

--- bootloaders/riscv-pk/pk/pk.c
+++ bootloaders/riscv-pk/pk/pk.c
@@ -134,7 +134,7 @@ static void run_loaded_program(size_t argc, char** argv, uintptr_t kstack_top)
 
   trapframe_t tf;
   init_tf(&tf, current.entry, stack_top);
-  __clear_cache(0, 0);
+  asm volatile ("fence.i");
   write_csr(sscratch, kstack_top);
   start_user(&tf);
 }
